80a110a34592f498a545bca78da9bd0ca27ca402
Diogo Silva <diogosantosdasilva6@gmail.com>
2022-08-22 22:46:25 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,30 @@ sudo apt update && sudo apt upgrade;
 
 echo -e "[*] Iniciando Instalação."
 
+## Extensões
+array=(
+sound-output-device-chooser@kgshank.net
+dash-to-panel@jderose9.github.com
+user-theme@gnome-shell-extensions.gcampax.github.com
+blur-my-shell@aunetx clipboard-history@alexsaveau.dev
+emoji-selector@maestroschan.fr
+gsconnect@andyholmes.github.io
+just-perfection-desktop@just-perfection
+soft-brightness@fifi.org
+rounded-window-corners@yilozt)
+
+for i in "${array[@]}"
+do
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=${i}" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+    wget -O ${i}.zip "https://extensions.gnome.org/download-extension/${i}.shell-extension.zip?version_tag=$VERSION_TAG"
+    gnome-extensions install --force ${EXTENSION_ID}.zip
+    if ! gnome-extensions list | grep --quiet ${i}; then
+        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${i}
+    fi
+    gnome-extensions enable ${i}
+    rm ${EXTENSION_ID}.zip
+done
+
 ## Iniciando Flatpak
 clear
 echo -e "[*] Iniciando Flatpak"
