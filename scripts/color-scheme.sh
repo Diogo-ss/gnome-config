8494cd6c036f874b5a2d35a7f6547881158439c3
Diogo Silva <diogosantosdasilva6@gmail.com>
2022-09-04 13:01:06 -0300

    Update color-scheme.sh

--- scripts/color-scheme.sh
+++ scripts/color-scheme.sh
@@ -2,11 +2,13 @@
 
 ## Tema por https://github.com/Peter-van-der-Velde/Two-Dark
 
+git clone https://github.com/Peter-van-der-Velde/Two-Dark.git
+
 mkdir -pv ~/.local/share/gtksourceview-3.0/styles/
 mkdir -pv ~/.local/share/gtksourceview-4/styles/
 
-cp -v ./theme_two_dark.xml ~/.local/share/gtksourceview-3.0/styles/
-cp -v ./theme_two_dark.xml ~/.local/share/gtksourceview-4/styles/
+cp -v ./Two-Dark/theme_two_dark.xml ~/.local/share/gtksourceview-3.0/styles/
+cp -v ./Two-Dark/theme_two_dark.xml ~/.local/share/gtksourceview-4/styles/
 
 ## Terminal por https://github.com/denysdovhan/one-gnome-terminal
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
